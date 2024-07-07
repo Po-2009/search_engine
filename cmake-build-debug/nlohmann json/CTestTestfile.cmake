014202f6b6ee65aaffc3bf6aace45c8fd6404a59
Po_ <mu312933@gmail.com>
2024-07-07 20:31:43 +0400

    delete useless directory

--- cmake-build-debug/nlohmann json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/poladmagerramli/CLionProjects/search_engine/nlohmann json
-# Build directory: /Users/poladmagerramli/CLionProjects/search_engine/cmake-build-debug/nlohmann json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
